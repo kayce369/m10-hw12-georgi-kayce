17eefdd7eecfd9fb81d51819ded016c036d1b896
Kayce Georgi <georgi.kayce@ufl.edu>
2021-12-05 16:21:41 -0500

    created car class

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // YOUR CODE HERE
+
+class Car {
+    constructor (make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+    honk() {
+        console.log('beep beep!')
+    }
+    performMaintenance() {
+        setTimeout(() => {
+            console.log ('Maintenance Complete')
+        }, 3000);
+    }
+}
